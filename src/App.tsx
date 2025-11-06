91d606b0322223c1d5a3f14bb5ede540cb670f47
JonathanReis-bit <143040256+JonathanReis-bit@users.noreply.github.com>
2025-11-06 14:23:48 -0300

    cria cartinha dos jogadores

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Jogador from './Jogador'
 
 function App() {
 
   return (
     <>
-  
-    </>
+      <Jogador nome="Gabi" posicao="ATA" nota={2.3} preco={13.5} />
+     </>
   )
 }
 
--- src/Jogador.tsx
+++ src/Jogador.tsx
@@ -9,10 +9,20 @@ interface JogadorProps {
 export default function Jogador ({nome, posicao, nota, preco}: JogadorProps) {
     return (
         <div className="jogador">
-            <div className="nome">{nome}</div>
-            <div className="posicao">{posicao}</div>
-            <div className="nota">{nota?.toLocaleString()}</div>
-            <div className="precoJogador">{preco?.toLocaleString()}</div>
+            <div className="topo">
+                <div className="nome">{nome}</div>
+                <div className="posicao">{posicao}</div>
+            </div>
+            <div className="meio">
+                <img src="imagens/uniforme.png" alt="Unifrome" className="uniforme"></img>
+            </div>
+            <div className="base">
+                <div className={`nota ${nota! >= 0 ? "alta" :  "baixa"}`}>
+                    {nota?.toFixed(2)}
+                </div>
+            </div>
+
+            <div className="precoJogador">{preco?.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}</div>
         </div>
     )
 }
